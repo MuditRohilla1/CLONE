490c098c510033c8c819b6af0e3b6214ed1ca2f3
muditrohilla <rohillamudit679@gmail.com>
2024-09-12 15:28:57 +0530

    2-[AUTH-ROUTES] completed

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
-
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); //to parse request.body
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () =>{
-    console.log(`Server is running on port ${PORT}`);
-    connectMongoDB();
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectMongoDB();
+});
