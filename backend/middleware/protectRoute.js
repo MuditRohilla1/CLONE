b4423bf770bae1b33f8b92e43c24763bf3ba5f10
muditrohilla <rohillamudit679@gmail.com>
2024-09-13 20:29:10 +0530

    3-[USER ROUETES] completed

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -1,33 +1,29 @@
 import User from "../models/user.model.js";
-import jwt from "jsonwebtoken"
-
+import jwt from "jsonwebtoken";
 
 export const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
     if (!token) {
-      return res
-        .status(401)
-        .json({ success: false, message: "You are not loggedIn" });
+      return res.status(401).json({ error: "Unauthorized: No Token Provided" });
     }
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     if (!decoded) {
-      return res.status(401).json({ success: false, message: "Invalid Token" });
+      return res.status(401).json({ error: "Unauthorized: Invalid Token" });
     }
 
     const user = await User.findById(decoded.userId).select("-password");
 
     if (!user) {
-      return res
-        .status(404)
-        .json({ success: false, message: "User not found" });
+      return res.status(404).json({ error: "User not found" });
     }
-    
+
     req.user = user;
     next();
-  } catch (error) {
-    console.error("Error in protectRoute Middleware", error.message);
-    res.status(500).json({ error: "Internal Server Error" });
+  } catch (err) {
+    console.log("Error in protectRoute middleware", err.message);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 };
