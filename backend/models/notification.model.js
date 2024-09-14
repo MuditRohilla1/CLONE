a78e1e90b3a6d47f10a069ea4629c3713310d6a3
muditrohilla <rohillamudit679@gmail.com>
2024-09-14 22:58:49 +0530

    4-[POSTS] completed

--- backend/models/notification.model.js
+++ backend/models/notification.model.js
@@ -14,7 +14,7 @@ const notificationSchema = new mongoose.Schema(
     },
     type: {
       type: String,
-      enum: ["follow", "like"],
+      enum: ["follow", "like", "comment"],
       required: true,
     },
     read: {
